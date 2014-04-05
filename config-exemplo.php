e3a8f5965306e1e32b3b3bb180c2c318a760966a
Victor Diniz <blek.net@gmail.com>
2014-04-05 17:17:57 -0300

    Adicionado arquivo de config de exemplo

---
+++ config-exemplo.php
@@ -0,0 +1,14 @@
+<?php 
+	$dbconf = array(
+		'host' => 'localhost',
+		'username' => 'username',
+		'password' => 'password',
+		'db' => 'database'
+	);
+
+	$siteconf = array(
+		'title' => 'titulo do site',
+		'location' => 'endereconosite/',
+		'authpwd' => 'md5hashdasenhadeautentificação'
+	);
+ ?>
