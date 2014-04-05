dbe826b654f402b7f76769e816329be62b59e8be
Victor Diniz <blek.net@gmail.com>
2014-04-05 17:23:14 -0300

    Adicionado arquivo com SQL para criar as tabelas da DB

---
+++ quotes.sql
@@ -0,0 +1,19 @@
+-- Rode essa joça, obtenha tabelas
+
+CREATE TABLE IF NOT EXISTS `largequotes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `conteudo` varchar(4096) NOT NULL,
+  `legenda` varchar(256) NOT NULL,
+  `stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `aprovado` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+CREATE TABLE IF NOT EXISTS `smallquotes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `conteudo` varchar(256) NOT NULL,
+  `legenda` varchar(256) NOT NULL,
+  `stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `aprovado` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=88 ;
