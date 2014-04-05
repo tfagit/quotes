9ab271cc5935f430442d22c2ca2265750d3b1a37
Victor Diniz <blek.net@gmail.com>
2014-04-05 20:29:26 -0300

    Limpadão (esqueci de adicionar um arquivo, xd)

---
+++ views/modquote.view.php
@@ -0,0 +1,12 @@
+<li>
+	<form action="?page=admin<?= $extended ? "&ext=1" : '' ?> method="post">
+		ID: <?=$quote['id']?><br>
+		Conte&uacute;do:<br>
+		<?= $extended ? 
+			'<pre><textarea name="conteudolarge" rows="2" cols="60">' . str_replace('"', '&quot;', $quote['conteudo']) . '</textarea></pre>'
+			: '<input type="text" name="conteudosmall" size="64" value="' . str_replace('"', '&quot;', $quote['conteudo']) . '"><br>'
+		?>
+		Legenda:<br><input type="text" name="legenda" size="64" value="<?=str_replace('"', '&quot;', $quote['legenda'])?>"><br>
+		<input type="submit" name="rating" value="Aprovar"><input type="submit" name="rating" value="Reprovar"><input type="hidden" name="id" value="<?=$quote['id']?>">
+	</form>
+</li>
